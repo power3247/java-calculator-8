81e0c03a86347cc871199ef54dbb03088f105fe5
power <posmane5@gmail.com>
2025-10-20 22:08:07 +0900

    refactor : 요구 조건에 맞게 출력 및 answer 변수타입 수정.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,9 +7,10 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputText = readLine();
         StringCalculator calculator = new StringCalculator();
-        int answer = calculator.add(inputText);
+        Long answer = calculator.add(inputText);
         System.out.println("결과 : " + answer);
     }
 }
