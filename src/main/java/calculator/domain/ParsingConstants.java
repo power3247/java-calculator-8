777656fcc8cbfbd624ecc8b7b01dbce0c6379b47
power <posmane5@gmail.com>
2025-10-20 21:43:13 +0900

    fix : ParsingConstants 유틸 클래스의 CUSTOM_DELIMITER_SUFFIX값을 프로그램이 작동하도록 수정

--- src/main/java/calculator/domain/ParsingConstants.java
+++ src/main/java/calculator/domain/ParsingConstants.java
@@ -6,5 +6,5 @@ public final class ParsingConstants {
     }
 
     public static final String CUSTOM_DELIMITER_PREFIX = "//";
-    public static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
 }
