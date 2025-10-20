20236e8bd19c20a688412ec4603cac1f9f483338
power <posmane5@gmail.com>
2025-10-20 19:47:44 +0900

    fix : Validator 클래스의 메소드들에 trim 메소드 제거, 잘못된 공백 입력이 허용되어 예측 불가능한 결과가 야기될수 있다고 판단함.

--- src/main/java/calculator/validator/Validator.java
+++ src/main/java/calculator/validator/Validator.java
@@ -4,14 +4,14 @@ public class Validator {
 
     private Validator() {}
     public static void validateInputTextEmpty(String inputText) {
-        if (inputText == null || inputText.trim().isEmpty()) {
+        if (inputText == null || inputText.isEmpty()) {
             throw new IllegalArgumentException("빈 문자열이나 공백은 입력값이 될수 없습니다");
         }
     }
 
     public static void validateNoEmptyElement(String[] stringNumbers) {
         for (String numberStr : stringNumbers) {
-            if (numberStr.trim().isEmpty()) {
+            if (numberStr.isEmpty()) {
                 throw new IllegalArgumentException("빈 문자열이나 공백은 덧셈에 적합하지 않습니다.");
             }
         }
