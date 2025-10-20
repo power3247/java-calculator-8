9f5e8a37660003c28982e397c15b5bfadce80239
power <posmane5@gmail.com>
2025-10-20 18:20:48 +0900

    feat : 입출력 기능과 함께 프로그램 진행 코드 구현.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import calculator.domain.StringCalculator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputText = readLine();
+        StringCalculator calculator = new StringCalculator();
+        int answer = calculator.add(inputText);
+        System.out.println("결과 : " + answer);
     }
 }
