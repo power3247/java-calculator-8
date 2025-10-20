9bbc217615dbc416aedf54f4945143e89611be1f
power <posmane5@gmail.com>
2025-10-20 16:38:10 +0900

    feat : Delimiters 객체 생성, 각종 상수 및 상태표현필드 생성, 좀더 명시적인 이름으로 구현.

--- README.md
+++ README.md
@@ -4,12 +4,13 @@
   - [x] 그 구분자를 활용해서 문자열을 구분해서 합구하기 StringCalculator.add()
     - [x] 숫자 문자열과 구분자를 받아 구분 StringCalculator.splitNumbers() 
     - [x] 구분된 문자열 배열을 정수형 리스트로 변환 StringCalculator.parseNumbers() 
-    - [x] 숫자의 합을 구하기 StringCalculator.sum()
-- [ ] 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 
+    - [x] 숫자의 합을 구하기 StringCalculator.sum() 
+- [x] 입력받은 문자열을 커스텀 구분자 부분과, 합할 숫자 부분을 나누기 StringCalculator.extractNumbersText()
 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다. Delimiters
-  - [x] 입력받은 문자열을 커스텀 구분자 부분과, 합할 숫자 부분을 나누기 StringCalculator.extractNumbersText()
-  - [ ] 입력받은 문자열 앞부분 에서 "//"와 "\n" 사이의 문자(커스텀 구분자) 파악및 저장 Delimiters.findCustomDelimiter()
-  - [ ] 커스텀 구분자를 쉼표와 콜론과 함께 구분자가 저장된 자료구조에 저장 Delimiters.delimiters[]
+  - [x] Delimiters 객체 생성, 각종상태표현, 좀더 명시적인 이름 Delimiters.createDelimiters()
+  - [ ] 입력받은 문자열 앞부분 에서 "//"와 "\n" 사이의 커스텀 구분자 추가 Delimiters.setCustomDelimiter()
+    - [ ] 입력받은 문자열에서 커스텀 구분자 추출 Delimiters.extractCustomDelimiter()
+  - [ ] 커스텀 구분자를 저장한 리스트 Delimiters.delimiters
 - [ ] 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
   - [ ] 올바른 값인지 체크
   - [ ] 잘못된 값 정의 (값의 정의)
--- src/main/java/calculator/domain/Delimiters.java
+++ src/main/java/calculator/domain/Delimiters.java
@@ -1,20 +1,36 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Delimiters {
-    String[] delimiters;
-    private Delimiters(String inputText){}
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+    private static final String BASIC_DELIMITER_COMMA = ",";
+    private static final String BASIC_DELIMITER_COLON = ":";
 
-    public String toRegex(){
-        return delimitersRegex;
+    private final List<String> delimiters = new ArrayList<>(List.of(BASIC_DELIMITER_COMMA, BASIC_DELIMITER_COLON));
+    private final String inputText;
+
+    private Delimiters(String inputText) {
+        this.inputText = inputText;
     }
 
-    public static Delimiters createDelimiters(String inputText){
-        return new Delimiters(inputText);
+    public static Delimiters createDelimiters(String inputText) {
+        Delimiters delimiters = new Delimiters(inputText);
+        if (inputText.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            delimiters.setCustomDelimiter();
+        }
+        return delimiters;
     }
 
-    public String splitCustomDelimiter() {
+    private void setCustomDelimiter() {
+    }
+
+    private String extractCustomDelimiter() {
         return "";
     }
+
     public String getRegex() {
         return "";
     }
